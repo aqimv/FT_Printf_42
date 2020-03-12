86f2f6d40a699ae036ff1cd182f52225994e8178
skennith1906 <skennith@student.21-school.ru>
2020-03-12 15:07:27 +0300

    start

---
+++ includes/ft_printf.h
@@ -0,0 +1,9 @@
+
+#ifndef PRINTF_FT_PRINTF_H
+# define PRINTF_FT_PRINTF_H
+
+# include <stdarg.h>
+
+
+
+#endif
---
+++ srcs/ft_printf.c
@@ -0,0 +1,9 @@
+
+#include "ft_printf.h"
+
+int		ft_printf(char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+}
